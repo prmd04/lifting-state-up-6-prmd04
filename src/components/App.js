09357aed1ecb6037035b62b257df7e08f8516e03
pramod ade <prmdextracthere@gmail.com>
2025-09-07 11:23:54 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,46 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const initalTodo = [
+    {task:"Learn React",isComplete:false},
+    {task:"Build a React App",isComplete:false},
+    {task:"Deploy a React App",isComplete:false}
+  ];
+
+  const [todos,setTodos] = useState(initalTodo);
+
+  const handleComplete = (index) =>{
+    const updateTodo = todos.map((item,i)=>{
+      return i===index ?{...item,isComplete:true}:item;
+    })
+    setTodos(updateTodo);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <TodoList array={todos} handleComplete={handleComplete}></TodoList>
     </div>
   )
 }
 
+
+const TodoList = ({array,handleComplete})=>{
+  return(
+    <div>
+    {array.map((list,index)=>{
+      return (list.isComplete === false) ?
+      (<div key={index}>
+        <p>{list.task} <button onClick={()=>handleComplete(index)}>complete</button></p>
+      </div>):
+      (<div key={index}>
+        <p>{list.task}</p>
+
+      </div>)
+    })}
+    </div>
+    
+  )
+}
 export default App
